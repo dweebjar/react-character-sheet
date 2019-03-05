eb2e1acda71ab39748964b9a02359a72363008a8
Jake Crump <dweebjar@gmail.com>
2019-03-04 18:09:06 -0600

    Publish site to gh-pages.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      str: 10,
+      dex: 10,
+      con: 10,
+      int: 10,
+      wis: 10,
+      cha: 10
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  calcProf(event) {
+    
+  }
+
+  reset() {
+    this.setState({
+      str: 10,
+      dex: 10,
+      con: 10,
+      int: 10,
+      wis: 10,
+      cha: 10
+    });
+  }
+
   render() {
+    const strSkill = Math.floor((this.state.str - 10) / 2);
+    const dexSkill = Math.floor((this.state.dex - 10) / 2);
+    const conSkill = Math.floor((this.state.con - 10) / 2);
+    const intSkill = Math.floor((this.state.int - 10) / 2);
+    const wisSkill = Math.floor((this.state.wis - 10) / 2);
+    const chaSkill = Math.floor((this.state.cha - 10) / 2);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="Title">Player Character Sheet</h1>
+        <div className="Content">
+          <div className="Attributes">
+            <ul>
+              <li>Str: </li>
+              <li>Dex: </li>
+              <li>Con: </li>
+              <li>Int: </li>
+              <li>Wis: </li>
+              <li>Cha: </li>
+            </ul>
+            <ul>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.str} type="number" step="1" max="20" min="0" name="str"></input></li>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.dex} type="number" step="1" max="20" min="0" name="dex"></input></li>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.con} type="number" step="1" max="20" min="0" name="con"></input></li>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.int} type="number" step="1" max="20" min="0" name="int"></input></li>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.wis} type="number" step="1" max="20" min="0" name="wis"></input></li>
+              <li><input onChange = {this.handleChange.bind(this)} value={this.state.cha} type="number" step="1" max="20" min="0" name="cha"></input></li>
+            </ul>
+          </div>
+          <div className="Skills">
+            <ul>
+              <li>Acrobatics: </li>
+              <li>Animal Handling: </li>
+              <li>Arcana: </li>
+              <li>Athletics: </li>
+              <li>Deception: </li>
+              <li>History: </li>
+              <li>Insight: </li>
+              <li>Intimidation: </li>
+              <li>Investigation: </li>
+            </ul>
+            <ul>
+              <li>{dexSkill}</li>
+              <li>{wisSkill}</li>
+              <li>{intSkill}</li>
+              <li>{strSkill}</li>
+              <li>{chaSkill}</li>
+              <li>{intSkill}</li>
+              <li>{wisSkill}</li>
+              <li>{chaSkill}</li>
+              <li>{intSkill}</li>
+            </ul>
+            <ul>
+              <li>Medicine: </li>
+              <li>Nature: </li>
+              <li>Perception: </li>
+              <li>Performance: </li>
+              <li>Persuasion: </li>
+              <li>Religion: </li>
+              <li>Sleight of Hand: </li>
+              <li>Stealth: </li>
+              <li>Survival: </li>
+            </ul>
+            <ul>
+              <li>{wisSkill}</li>
+              <li>{intSkill}</li>
+              <li>{wisSkill}</li>
+              <li>{chaSkill}</li>
+              <li>{chaSkill}</li>
+              <li>{intSkill}</li>
+              <li>{dexSkill}</li>
+              <li>{dexSkill}</li>
+              <li>{wisSkill}</li>
+            </ul>
+          </div>
+        </div>
+        <div id="btns">
+          <button onClick = {this.reset.bind(this)}>Reset</button>
+        </div>
       </div>
     );
   }
